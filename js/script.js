2ea6a8c0ec506011edc8ded8dc24fd7bf85b7c74
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-03-25 17:07:20 +0100

    feat: creation of user inputs

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 // test
 // alert("ciao")
+
+
+// INPUT
+// Richista età utente
+const userAge = prompt ("Si prega di inserire la propria età"); //string | null
+
+// Log della console
+console.log(userAge, typeof userAge);
+
+// Richiesta km da percorrere
+const numKM = prompt ("Inserire il numero di kilometri che si vuole percorrere");  //string | null
+
+// Log della console
+console.log(numKM, typeof numKM);
+
+// Valore euro per km
+const valEurKm = 0.21;
+
+// Log della console
+console.log(valEurKm, typeof valEurKm);
+
+
